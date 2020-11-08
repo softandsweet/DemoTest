90baf712af157dfb5140331c1d1083d9da3df94f
softandsweet <yuguigoudemimei@163.com>
2020-11-08 20:59:54 +0800

    第一次提交

---
+++ src/Dgr.java
@@ -0,0 +1,15 @@
+public class Dgr {
+    public void setName(String 打工人) {
+    }
+    public void setGjl(int a){
+        a=40;
+    }
+    public void setFyl(int b){
+        b=200;
+    }
+    public void setLife(int c){
+        c=1000;
+    }
+
+
+}
